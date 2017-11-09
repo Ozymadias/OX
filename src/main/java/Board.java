f7bdc736e3f957584328f546c4100ad603d19d1a
Dominik <saidamyzo@gmail.com>
2017-11-09 23:47:41 +0100

    Manage different winning conditions

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -4,6 +4,14 @@ class Board {
     private PlayerSign[][] board;
     private int numberOfRows;
     private int numberOfColumns;
+    private int winningNumber;
+
+    Board(int numberOfRows, int numberOfColumns, int winningNumber) {
+        this.board = new PlayerSign[numberOfRows][numberOfColumns];
+        this.numberOfColumns = numberOfColumns;
+        this.numberOfRows = numberOfRows;
+        this.winningNumber = winningNumber;
+    }
 
     Board(int numberOfRows, int numberOfColumns) {
         this.board = new PlayerSign[numberOfRows][numberOfColumns];
@@ -12,23 +20,61 @@ class Board {
     }
 
     Optional<PlayerSign> getWinner() {
+        Optional<PlayerSign> sign1 = checkRows();
+        if (sign1.isPresent()) return sign1;
+        Optional<PlayerSign> sign = checkColumns();
+        if (sign.isPresent()) return sign;
+        return Optional.empty();
+    }
+
+    private Optional<PlayerSign> checkRows() {
         for (int i = 0; i < numberOfRows; i++) {
-            Optional<PlayerSign> sign = checkIfContainsOnlySameSigns(board[i]);
+            Optional<PlayerSign> sign = checkIfRowsContainsOnlySameSigns(i);
             if (sign.isPresent())
                 return sign;
         }
         return Optional.empty();
     }
 
-    private Optional<PlayerSign> checkIfContainsOnlySameSigns(PlayerSign[] row) {
-        PlayerSign sign = row[0];
-        if (sign == null)
-            return Optional.empty();
-        for (int j = 1; j < numberOfColumns; j++) {
-            if (row[j] != sign)
-                return Optional.empty();
+    private Optional<PlayerSign> checkColumns() {
+        for (int i = 0; i < numberOfColumns; i++) {
+            Optional<PlayerSign> sign = checkIfColumnsContainsOnlySameSigns(i);
+            if (sign.isPresent())
+                return sign;
         }
-        return Optional.of(sign);
+        return Optional.empty();
+    }
+
+    private Optional<PlayerSign> checkIfColumnsContainsOnlySameSigns(int columnNumber) {
+        int numberOfOccurrence = 0;
+        PlayerSign sign = null;
+        for (int i = 0; i < numberOfRows; i++) {
+            if (board[i][columnNumber] == sign) {
+                numberOfOccurrence++;
+                if (numberOfOccurrence == winningNumber && sign != null)
+                    return Optional.of(sign);
+            } else {
+                sign = board[i][columnNumber];
+                numberOfOccurrence = 1;
+            }
+        }
+        return Optional.empty();
+    }
+
+    private Optional<PlayerSign> checkIfRowsContainsOnlySameSigns(int rowNumber) {
+        int numberOfOccurrence = 0;
+        PlayerSign sign = null;
+        for (int i = 0; i < numberOfRows; i++) {
+            if (board[rowNumber][i] == sign) {
+                numberOfOccurrence++;
+                if (numberOfOccurrence == winningNumber && sign != null)
+                    return Optional.of(sign);
+            } else {
+                sign = board[rowNumber][i];
+                numberOfOccurrence = 1;
+            }
+        }
+        return Optional.empty();
     }
 
     void put(PlayerSign playerSign, int rowNumber, int columnNumber) {
--- src/test/java/BoardTest.java
+++ src/test/java/BoardTest.java
@@ -15,8 +15,26 @@ public class BoardTest {
 
     @Test
     public void ifBoardHasRowWithSameSignsThisSignShouldWin() {
-        Board board = new Board(3, 3);
+        Board board = new Board(3, 3, 3);
         for(int i = 0; i < 3; i++)
+            board.put(PlayerSign.X, 0, i);
+        Optional<PlayerSign> winner = board.getWinner();
+        assertEquals(winner, Optional.of(PlayerSign.X));
+    }
+
+    @Test
+    public void ifBoardHasColumnWithSameSignsThisSignShouldWin() {
+        Board board = new Board(3, 3, 3);
+        for(int i = 0; i < 3; i++)
+            board.put(PlayerSign.X, i, 0);
+        Optional<PlayerSign> winner = board.getWinner();
+        assertEquals(winner, Optional.of(PlayerSign.X));
+    }
+
+    @Test
+    public void ifTwoSignsWinsGameTwoSignsInSameRowShouldWin() {
+        Board board = new Board(3, 3, 2);
+        for(int i = 0; i < 2; i++)
             board.put(PlayerSign.X, 0, i);
         Optional<PlayerSign> winner = board.getWinner();
         assertEquals(winner, Optional.of(PlayerSign.X));
