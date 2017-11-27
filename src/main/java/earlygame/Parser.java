b2da3d61def388a4882b963ed3b538715093fe26
Dominik <saidamyzo@gmail.com>
2017-11-27 01:46:41 +0100

    Group validation of board size

--- src/main/java/earlygame/Parser.java
+++ src/main/java/earlygame/Parser.java
@@ -1,11 +1,9 @@
 package earlygame;
 
-import coregame.Position;
-
 class Parser {
-    static Position parse(String s) {
+    static Coordinates parse(String s) {
         String[] coordinates = s.split("\\s+");
         int size = coordinates.length;
-        return new Position(Integer.parseInt(coordinates[size - 2]), Integer.parseInt(coordinates[size - 1]));
+        return new Coordinates(Integer.parseInt(coordinates[size - 2]), Integer.parseInt(coordinates[size - 1]));
     }
 }
